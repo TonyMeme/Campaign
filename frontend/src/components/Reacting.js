2e0c466cb3b9fb55dc9383000b1aa6cc33626a5f
TonyMeme <tonychong203@gmail.com>
2023-06-23 13:57:06 +0200

    Updated photos

--- frontend/src/components/Reacting.js
+++ frontend/src/components/Reacting.js
@@ -6,50 +6,50 @@ function Reacting({value, text, color}) {
       <span>
         <i style={{ color }} className={
             value >= 1
-                ? 'fa-solid fa-heart'
+                ? 'fa-solid fa-star'
                 : value >= 0.5
-                    ? 'fa-solid fa-heart-half'
-                    : 'fa-solid fa-heart'
+                    ? 'fa-solid fa-star-half'
+                    : 'fa-solid fa-star'
         }>
         </i>
       </span>
       <span>
         <i style={{ color }} className={
             value >= 2
-                ? 'fa-solid fa-heart'
+                ? 'fa-solid fa-star'
                 : value >= 1.5
-                    ? 'fa-solid fa-heart-half'
-                    : 'fa-solid fa-heart'
+                    ? 'fa-solid fa-star-half'
+                    : 'fa-solid fa-star'
         }>
         </i>
       </span>
       <span>
         <i style={{ color }} className={
             value >= 3
-                ? 'fa-solid fa-heart'
+                ? 'fa-solid fa-star'
                 : value >= 2.5
-                    ? 'fa-solid fa-heart-half'
-                    : 'fa-solid fa-heart'
+                    ? 'fa-solid fa-star-half'
+                    : 'fa-solid fa-star'
         }>
         </i>
       </span>
       <span>
         <i style={{ color }} className={
             value >= 4
-                ? 'fa-solid fa-heart'
+                ? 'fa-solid fa-star'
                 : value >= 3.5
-                    ? 'fa-solid fa-heart-half'
-                    : 'fa-solid fa-heart'
+                    ? 'fa-solid fa-star-half'
+                    : 'fa-solid fa-star'
         }>
         </i>
       </span>
       <span>
         <i style={{ color }} className={
             value >= 5
-                ? 'fa-solid fa-heart'
+                ? 'fa-solid fa-star'
                 : value >= 4.5
-                    ? 'fa-solid fa-heart-half'
-                    : 'fa-solid fa-heart'
+                    ? 'fa-solid fa-star-half'
+                    : 'fa-solid fa-star'
         }>
         </i>
       </span>
